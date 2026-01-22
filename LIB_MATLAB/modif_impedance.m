df0178038904a32ec09d46669168591c1fdb1b91
japchavez <jap.chavez@gmail.com>
2026-01-22 16:59:00 +0100

    Add files via upload

---
+++ LIB_MATLAB/modif_impedance.m
@@ -0,0 +1,269 @@
+function out = model
+%
+% modif_impedance.m
+%
+% Model exported on May 5 2017, 13:56 by COMSOL 5.2.0.166.
+
+import com.comsol.model.*
+import com.comsol.model.util.*
+
+model = ModelUtil.create('Model');
+
+model.modelPath('D:\Nicolas\Dropbox\Dropbox\0_THESE\CODE\LIB_MATLAB');
+
+model.label('parabole.mph');
+
+model.comments(['Untitled\n\nno Comment']);
+
+model.param.set('ringAnodeSpace', '0.3[mm]');
+model.param.set('x0', '0');
+model.param.set('nerveD', '0.00135[m]');
+model.param.set('fasciculeD', '0.00040000[m]');
+model.param.set('fasciculeLayer', '0.00005000[m]');
+model.param.set('sigmaSaline', '2.0000000000[S/m]');
+model.param.set('sigmaEpineurium', '0.0080000000[S/m]');
+model.param.set('sigmaPerineurium', '0.0033600000[S/m]');
+model.param.set('sigmaFasciculexy', '0.0800000000[S/m]');
+model.param.set('sigmaFasciculez', '0.5000000000[S/m]');
+model.param.set('sigmaEXT', '0.1600000000[m]');
+model.param.set('ring1D', '0.001950[m]');
+model.param.set('ring2D', '0.002600[m]');
+model.param.set('ringD', '0.002000[m]');
+model.param.set('nerveL', '0.040000[m]');
+model.param.set('ringTorusD', '0.000030[m]');
+model.param.set('ringCuffD', '0.0028500000[m]');
+model.param.set('courant', '1.00000[A]');
+model.param.set('cuffL', '0.005100[m]');
+model.param.set('LayerBC', '100[mm]');
+model.param.set('alpha', '-86.350000[m]');
+model.param.set('sigmaConjonctive', '0.160000[m]');
+
+model.modelNode.create('comp1');
+
+model.geom.create('geom1', 3);
+
+model.mesh.create('mesh1', 'geom1');
+
+model.geom('geom1').selection.create('csel7', 'CumulativeSelection');
+model.geom('geom1').selection('csel7').label('ring2Domain');
+model.geom('geom1').selection.create('csel8', 'CumulativeSelection');
+model.geom('geom1').selection('csel8').label('ring1Domain');
+model.geom('geom1').selection.create('csel3', 'CumulativeSelection');
+model.geom('geom1').selection('csel3').label('extBC');
+model.geom('geom1').selection.create('csel4', 'CumulativeSelection');
+model.geom('geom1').selection('csel4').label('fascicle');
+model.geom('geom1').selection.create('csel5', 'CumulativeSelection');
+model.geom('geom1').selection('csel5').label('nerveB');
+model.geom('geom1').selection.create('csel6', 'CumulativeSelection');
+model.geom('geom1').selection('csel6').label('fascicleDomain');
+model.geom('geom1').create('cyl1', 'Cylinder');
+model.geom('geom1').feature('cyl1').label('nerf');
+model.geom('geom1').feature('cyl1').set('r', 'nerveD/2');
+model.geom('geom1').feature('cyl1').set('selresult', 'on');
+model.geom('geom1').feature('cyl1').set('contributeto', 'csel5');
+model.geom('geom1').feature('cyl1').set('pos', {'0' '0' '-nerveL/2'});
+model.geom('geom1').feature('cyl1').set('h', 'nerveL');
+model.geom('geom1').create('cyl2', 'Cylinder');
+model.geom('geom1').feature('cyl2').label('milieu');
+model.geom('geom1').feature('cyl2').set('r', 'nerveD*2');
+model.geom('geom1').feature('cyl2').set('selresult', 'on');
+model.geom('geom1').feature('cyl2').set('contributeto', 'csel3');
+model.geom('geom1').feature('cyl2').set('layername', {'' '' '' '' ''});
+model.geom('geom1').feature('cyl2').set('layertop', true);
+model.geom('geom1').feature('cyl2').set('selresultshow', 'bnd');
+model.geom('geom1').feature('cyl2').set('pos', {'0' '0' '-nerveL'});
+model.geom('geom1').feature('cyl2').set('layerbottom', true);
+model.geom('geom1').feature('cyl2').set('h', 'nerveL*2');
+model.geom('geom1').create('tor2', 'Torus');
+model.geom('geom1').feature('tor2').label('ring2');
+model.geom('geom1').feature('tor2').set('rmin', 'ringTorusD/2');
+model.geom('geom1').feature('tor2').set('rmaj', 'ring2D/2');
+model.geom('geom1').feature('tor2').set('contributeto', 'csel7');
+model.geom('geom1').feature('tor2').set('pos', {'0' '0' '-ringD/2'});
+model.geom('geom1').create('cyl3', 'Cylinder');
+model.geom('geom1').feature('cyl3').label('cuff');
+model.geom('geom1').feature('cyl3').set('r', 'ringCuffD/2');
+model.geom('geom1').feature('cyl3').set('selresult', 'on');
+model.geom('geom1').feature('cyl3').set('pos', {'0' '0' '-cuffL/2'});
+model.geom('geom1').feature('cyl3').set('h', 'cuffL');
+model.geom('geom1').create('wp1', 'WorkPlane');
+model.geom('geom1').feature('wp1').set('unite', 'on');
+model.geom('geom1').feature('wp1').set('quickz', 'nerveL/2');
+model.geom('geom1').feature('wp1').geom.create('pol1', 'Polygon');
+model.geom('geom1').feature('wp1').geom.feature('pol1').set('x', '0.00064125  0.00063996  0.00063609  0.00062966   0.0006207  0.00060923  0.00059532    0.000579  0.00056036  0.00053945  0.00051638  0.00049123  0.00046409  0.00043509  0.00040434  0.00037196  0.00033808  0.00030284  0.00026638  0.00022885   0.0001904  0.00015118  0.00011135  7.1075e-05  3.0512e-05 -1.0174e-05 -5.0819e-05 -9.1259e-05 -0.00013133 -0.00017088 -0.00020973 -0.00024774 -0.00028476 -0.00032062  -0.0003552 -0.00038835 -0.00041993 -0.00044982  -0.0004779 -0.00050406 -0.00052818 -0.00055018 -0.00056997 -0.00058745 -0.00060258 -0.00061527 -0.00062549  -0.0006332 -0.00063835 -0.00064093 -0.00064093 -0.00063835  -0.0006332 -0.00062549 -0.00061527 -0.00060258 -0.00058745 -0.00056997 -0.00055018 -0.00052818 -0.00050406  -0.0004779 -0.00044982 -0.00041993 -0.00038835  -0.0003552 -0.00032063 -0.00028476 -0.00024774 -0.00020973 -0.00017088 -0.00013133 -9.1259e-05 -5.0819e-05 -1.0174e-05  3.0512e-05  7.1075e-05  0.00011135  0.00015118   0.0001904  0.00022885  0.00026638  0.00030284  0.00033808  0.00037196  0.00040434  0.00043509  0.00046409  0.00049123  0.00051638  0.00053945  0.00056036    0.000579  0.00059532  0.00060923   0.0006207  0.00062966  0.00063609  0.00063996  0.00064125');
+model.geom('geom1').feature('wp1').geom.feature('pol1').set('y', '1.0283e-20  4.0671e-05  8.1177e-05  0.00012136  0.00016105  0.00020009  0.00023833  0.00027561  0.00031177  0.00034669   0.0003802  0.00041219  0.00044251  0.00047106   0.0004977  0.00052235  0.00054489  0.00056523   0.0005833  0.00059902  0.00061233  0.00062317  0.00063151   0.0006373  0.00064052  0.00064117  0.00063923  0.00063472  0.00062766  0.00061806  0.00060598  0.00059146  0.00057456  0.00055534  0.00053389  0.00051028  0.00048462  0.00045702  0.00042757  0.00039639  0.00036363   0.0003294  0.00029384   0.0002571  0.00021932  0.00018066  0.00014127  0.00010132  6.0955e-05  2.0346e-05 -2.0346e-05 -6.0955e-05 -0.00010132 -0.00014127 -0.00018066 -0.00021932  -0.0002571 -0.00029384  -0.0003294 -0.00036363 -0.00039639 -0.00042757 -0.00045702 -0.00048462 -0.00051028 -0.00053389 -0.00055534 -0.00057456 -0.00059146 -0.00060598 -0.00061806 -0.00062766 -0.00063472 -0.00063923 -0.00064117 -0.00064052  -0.0006373 -0.00063151 -0.00062317 -0.00061233 -0.00059902  -0.0005833 -0.00056523 -0.00054489 -0.00052235  -0.0004977 -0.00047106 -0.00044251 -0.00041219  -0.0003802 -0.00034669 -0.00031177 -0.00027561 -0.00023833 -0.00020009 -0.00016105 -0.00012136 -8.1177e-05 -4.0671e-05 -1.4678e-19');
+model.geom('geom1').create('ext1', 'Extrude');
+model.geom('geom1').feature('ext1').set('reverse', true);
+model.geom('geom1').feature('ext1').setIndex('distance', 'nerveL', 0);
+model.geom('geom1').feature('ext1').set('selresult', 'on');
+model.geom('geom1').feature('ext1').set('contributeto', 'csel4');
+model.geom('geom1').feature('ext1').set('selresultshow', 'all');
+model.geom('geom1').feature('ext1').selection('input').set({'wp1'});
+model.geom('geom1').create('pc1', 'ParametricCurve');
+model.geom('geom1').feature('pc1').set('selresult', 'on');
+model.geom('geom1').feature('pc1').set('coord', {'0' '0' 's'});
+model.geom('geom1').feature('pc1').set('parmin', '5[mm]');
+model.geom('geom1').feature('pc1').set('parmax', 'nerveL/2');
+model.geom('geom1').create('ps1', 'ParametricSurface');
+model.geom('geom1').feature('ps1').set('selresult', 'on');
+model.geom('geom1').feature('ps1').set('coord', {'-alpha/1[m]*(s2)^2+s1' '0' 's2'});
+model.geom('geom1').feature('ps1').set('pos', {'0.05[mm]+ring1D/2' '0' 'ringD/2'});
+model.geom('geom1').feature('ps1').set('parmax1', '0.05[mm]');
+model.geom('geom1').feature('ps1').set('parmax2', '1.5[mm]');
+model.geom('geom1').create('rev1', 'Revolve');
+model.geom('geom1').feature('rev1').set('angtype', 'full');
+model.geom('geom1').feature('rev1').set('selresult', 'on');
+model.geom('geom1').feature('rev1').set('contributeto', 'csel8');
+model.geom('geom1').feature('rev1').set('revolvefrom', 'faces');
+model.geom('geom1').feature('rev1').set('axis3', {'0' '0' '1'});
+model.geom('geom1').feature('rev1').set('axistype', '3d');
+model.geom('geom1').feature('rev1').selection('inputface').named('ps1');
+model.geom('geom1').create('sel1', 'ExplicitSelection');
+model.geom('geom1').feature('sel1').label('ringpara1');
+model.geom('geom1').feature('sel1').selection('selection').set('rev1(1)', [1]);
+model.geom('geom1').create('pc2', 'ParametricCurve');
+model.geom('geom1').feature('pc2').set('selresult', 'on');
+model.geom('geom1').feature('pc2').set('coord', {'0' '0' 's'});
+model.geom('geom1').feature('pc2').set('parmin', '-2[cm]');
+model.geom('geom1').feature('pc2').set('parmax', '2[cm]');
+model.geom('geom1').create('pc3', 'ParametricCurve');
+model.geom('geom1').feature('pc3').set('selresult', 'on');
+model.geom('geom1').feature('pc3').set('coord', {'nerveD/4' '0' 's'});
+model.geom('geom1').feature('pc3').set('parmin', '-2[cm]');
+model.geom('geom1').feature('pc3').set('parmax', '2[cm]');
+model.geom('geom1').create('ballsel1', 'BallSelection');
+model.geom('geom1').feature('ballsel1').label('AllDomain');
+model.geom('geom1').feature('ballsel1').set('condition', 'inside');
+model.geom('geom1').feature('ballsel1').set('r', '1');
+model.geom('geom1').create('intsel1', 'IntersectionSelection');
+model.geom('geom1').feature('intsel1').set('entitydim', '2');
+model.geom('geom1').feature('intsel1').label('NerveFascicleIntersection');
+model.geom('geom1').feature('intsel1').set('input', {'csel5' 'csel4'});
+model.geom('geom1').create('difsel1', 'DifferenceSelection');
+model.geom('geom1').feature('difsel1').set('entitydim', '2');
+model.geom('geom1').feature('difsel1').label('fasciclePeripheralB');
+model.geom('geom1').feature('difsel1').set('add', {'csel4'});
+model.geom('geom1').feature('difsel1').set('subtract', {'intsel1'});
+model.geom('geom1').create('difsel3', 'DifferenceSelection');
+model.geom('geom1').feature('difsel3').label('NerfWithoutFascicles');
+model.geom('geom1').feature('difsel3').set('add', {'cyl1'});
+model.geom('geom1').feature('difsel3').set('subtract', {'csel4'});
+model.geom('geom1').create('difsel2', 'DifferenceSelection');
+model.geom('geom1').feature('difsel2').label('RealDomain');
+model.geom('geom1').feature('difsel2').set('add', {'ballsel1'});
+model.geom('geom1').feature('difsel2').set('subtract', {'csel7' 'csel8'});
+
+% new
+model.geom('geom1').create('ballsel2', 'BallSelection');
+model.geom('geom1').feature('ballsel2').label('cuffSurfacePeiphery');
+model.geom('geom1').feature('ballsel2').set('entitydim', '2');
+model.geom('geom1').feature('ballsel2').set('groupcontang', 'on');
+model.geom('geom1').feature('ballsel2').set('posx', 'ringCuffD/2');
+model.geom('geom1').feature('ballsel2').set('r', '0.1[mm]');
+
+
+model.geom('geom1').run;
+
+model.view.create('view3', 2);
+
+model.material.create('mat4', 'Common', 'comp1');
+model.material.create('mat3', 'Common', 'comp1');
+model.material.create('mat1', 'Common', 'comp1');
+model.material.create('mat2', 'Common', 'comp1');
+model.material.create('mat5', 'Common', 'comp1');
+model.material('mat3').selection.named('geom1_cyl3_dom');
+model.material('mat1').selection.named('geom1_cyl1_dom');
+model.material('mat2').selection.named('geom1_csel4_dom');
+model.material('mat5').selection.named('geom1_difsel1');
+
+model.physics.create('ec', 'ConductiveMedia', 'geom1');
+model.physics('ec').selection.named('geom1_difsel2');
+model.physics('ec').create('ci1', 'ContactImpedance', 2);
+model.physics('ec').feature('ci1').selection.named('geom1_difsel1');
+model.physics('ec').create('dimp1', 'DistributedImpedance', 2);
+model.physics('ec').feature('dimp1').selection.named('geom1_csel3_bnd');
+model.physics('ec').create('term1', 'Terminal', 2);
+model.physics('ec').feature('term1').selection.named('geom1_csel7_bnd');
+model.physics('ec').create('term2', 'Terminal', 2);
+model.physics('ec').feature('term2').selection.named('geom1_csel8_bnd');
+model.physics('ec').create('ein2', 'ElectricInsulation', 2);
+model.physics('ec').feature('ein2').selection.set([5 6 193 216]);
+model.physics.create('cir', 'Circuit', 'geom1');
+model.physics('cir').create('I1', 'CurrentSourceCircuit', -1);
+model.physics('cir').create('IvsU1', 'ModelDeviceIV', -1);
+model.physics('cir').create('I2', 'CurrentSourceCircuit', -1);
+model.physics('cir').create('IvsU2', 'ModelDeviceIV', -1);
+
+model.mesh('mesh1').create('ftet1', 'FreeTet');
+
+model.view('view1').set('scenelight', 'off');
+model.view('view3').axis.set('ymin', '-0.5677165985107422');
+model.view('view3').axis.set('ymax', '1.5677165985107422');
+model.view('view3').axis.set('xmin', '-1.261846899986267');
+model.view('view3').axis.set('xmax', '1.261846899986267');
+
+model.material('mat4').label('Saline');
+model.material('mat4').propertyGroup('def').set('electricconductivity', {'sigmaSaline' '0' '0' '0' 'sigmaSaline' '0' '0' '0' 'sigmaSaline'});
+model.material('mat4').propertyGroup('def').set('relpermittivity', {'1' '0' '0' '0' '1' '0' '0' '0' '1'});
+model.material('mat3').label('conjonctiveTissue');
+model.material('mat3').propertyGroup('def').set('electricconductivity', {'sigmaEXT' '0' '0' '0' 'sigmaEXT' '0' '0' '0' 'sigmaEXT'});
+model.material('mat3').propertyGroup('def').set('relpermittivity', {'1' '0' '0' '0' '1' '0' '0' '0' '1'});
+model.material('mat1').label('epineurium');
+model.material('mat1').propertyGroup('def').set('electricconductivity', {'sigmaEpineurium' '0' '0' '0' 'sigmaEpineurium' '0' '0' '0' 'sigmaEpineurium'});
+model.material('mat1').propertyGroup('def').set('relpermittivity', {'1' '0' '0' '0' '1' '0' '0' '0' '1'});
+model.material('mat2').label('fascicle');
+model.material('mat2').propertyGroup('def').set('electricconductivity', {'sigmaFasciculexy' '0' '0' '0' 'sigmaFasciculexy' '0' '0' '0' 'sigmaFasciculez'});
+model.material('mat2').propertyGroup('def').set('relpermittivity', {'1' '0' '0' '0' '1' '0' '0' '0' '1'});
+model.material('mat5').label('perineurium');
+model.material('mat5').propertyGroup('def').set('electricconductivity', {'sigmaPerineurium' '0' '0' '0' 'sigmaPerineurium' '0' '0' '0' 'sigmaPerineurium'});
+model.material('mat5').propertyGroup('def').set('relpermittivity', {'1' '0' '0' '0' '1' '0' '0' '0' '1'});
+
+model.physics('ec').feature('ci1').set('ds', ['50[' native2unicode(hex2dec({'00' 'b5'}), 'unicode') 'm]']);
+model.physics('ec').feature('dimp1').set('ds', 'LayerBC');
+model.physics('ec').feature('dimp1').set('sigmabnd_mat', 'userdef');
+model.physics('ec').feature('dimp1').set('sigmabnd', 'sigmaConjonctive');
+model.physics('ec').feature('dimp1').set('epsilonrbnd_mat', 'userdef');
+model.physics('ec').feature('term1').set('TerminalType', 'Circuit');
+model.physics('ec').feature('term2').set('TerminalName', '0');
+model.physics('ec').feature('term2').set('TerminalType', 'Circuit');
+model.physics('cir').prop('CircuitSettings').set('CreateNodes', '0');
+model.physics('cir').feature('I1').set('Connections', {'1'; '0'});
+model.physics('cir').feature('I1').set('value', 'courant');
+model.physics('cir').feature('IvsU1').set('V_src', 'root.comp1.ec.V0_1');
+model.physics('cir').feature('IvsU1').set('Connections', {'1'; '0'});
+model.physics('cir').feature('I2').set('Connections', {'2'; '0'});
+model.physics('cir').feature('I2').set('value', '-courant');
+model.physics('cir').feature('IvsU2').set('V_src', 'root.comp1.ec.V0_0');
+model.physics('cir').feature('IvsU2').set('Connections', {'2'; '0'});
+
+model.mesh('mesh1').feature('size').set('hauto', 3);
+model.mesh('mesh1').feature('size').set('custom', 'on');
+model.mesh('mesh1').feature('size').set('hmin', '0.5E-4');
+model.mesh('mesh1').run;
+
+model.study.create('std1');
+model.study('std1').create('stat', 'Stationary');
+
+model.sol.create('sol1');
+model.sol('sol1').study('std1');
+model.sol('sol1').attach('std1');
+model.sol('sol1').create('st1', 'StudyStep');
+model.sol('sol1').create('v1', 'Variables');
+model.sol('sol1').create('s1', 'Stationary');
+model.sol('sol1').feature('s1').create('fc1', 'FullyCoupled');
+model.sol('sol1').feature('s1').create('i1', 'Iterative');
+model.sol('sol1').feature('s1').feature('i1').create('mg1', 'Multigrid');
+model.sol('sol1').feature('s1').feature.remove('fcDef');
+
+model.result.create('pg1', 'PlotGroup3D');
+model.result('pg1').create('mslc1', 'Multislice');
+
+model.sol('sol1').attach('std1');
+model.sol('sol1').feature('s1').feature('i1').set('linsolver', 'cg');
+model.sol('sol1').feature('s1').feature('i1').feature('mg1').set('prefun', 'amg');
+model.sol('sol1').runAll;
+
+model.result('pg1').label('Electric Potential (ec)');
+model.result('pg1').set('edges', 'off');
+
+out = model;
